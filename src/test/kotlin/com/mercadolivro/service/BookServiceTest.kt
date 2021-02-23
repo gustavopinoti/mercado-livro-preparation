6867c5d603dd5d6319c3c5b6119296af6ba0aa6a
DanielDonato <daniel_donato10@yahoo.com.br>
2021-02-22 21:45:25 -0300

    Add bookservice tests

---
+++ src/test/kotlin/com/mercadolivro/service/BookServiceTest.kt
@@ -0,0 +1,153 @@
+package com.mercadolivro.service
+
+import com.mercadolivro.enums.BookStatus
+import com.mercadolivro.enums.Profiles
+import com.mercadolivro.exception.BadRequestException
+import com.mercadolivro.model.BookModel
+import com.mercadolivro.model.CustomerModel
+import com.mercadolivro.repository.BookRepository
+import io.mockk.*
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertAll
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.ApplicationEventPublisher
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+
+@SpringBootTest
+class BookServiceTest {
+
+    //TODO Testes falhando
+    @MockK
+    private lateinit var bookRepository: BookRepository
+
+    @MockK
+    private lateinit var applicationEventPublisher: ApplicationEventPublisher
+
+    @InjectMockKs
+    private lateinit var bookService: BookService
+
+    @BeforeEach
+    fun setUp() = MockKAnnotations.init(this)
+
+    @Nested
+    inner class WhenCallingFindById {
+        @Test
+        fun `throws a error when has no book for id`() {
+            val bookId = 123
+            every { bookRepository.findById(bookId) } returns Optional.empty()
+
+            assertThrows(BadRequestException::class.java) {
+                bookService.findById(bookId)
+            }
+        }
+
+        @Test
+        fun `returns a book when has book for id`() {
+            val bookId = 123
+            every { bookRepository.findById(bookId) } returns Optional.of(buildBook(bookId))
+
+            val result = bookService.findById(bookId)
+
+            assertEquals(result, buildBook(bookId))
+        }
+    }
+
+    @Nested
+    inner class WhenCallingFindAll {
+        @Test
+        fun `returns all the books`() {
+            val books = listOf(buildBook(1), buildBook(2))
+            every { bookRepository.findAll() } returns books
+
+            val result = bookService.findAll()
+
+            assertEquals(result, books)
+        }
+    }
+
+    @Nested
+    inner class WhenCallingCreate {
+        @Test
+        fun `returns created book`() {
+            val book = buildBook(null)
+            val createdBook = buildBook(1)
+            every { bookRepository.save(book) } returns createdBook
+
+            val result = bookService.create(book)
+
+            assertEquals(result, createdBook)
+            verify(exactly = 1) { bookRepository.save(book) }
+        }
+    }
+
+    @Nested
+    inner class WhenCallingUpdate {
+        @Test
+        fun `returns updated book`() {
+            val bookId = 123
+            val book = buildBook(bookId)
+            val updatedBook = buildBook(bookId, name = "new name")
+            every { bookRepository.save(book) } returns updatedBook
+            every { bookRepository.findById(bookId) } returns Optional.of(updatedBook)
+
+            val result = bookService.update(book)
+
+            assertEquals(result, updatedBook)
+            verify(exactly = 1) { bookRepository.save(book) }
+        }
+    }
+
+    @Nested
+    inner class WhenCallingActive {
+        @Test
+        fun `returns all the active books`() {
+            val books = listOf(buildBook(1), buildBook(2))
+            every { bookRepository.findAll() } returns books
+
+            val result = bookService.findAll()
+
+            assertEquals(result, books)
+        }
+    }
+
+    @Nested
+    inner class WhenCallingPurchase() {
+        @Test
+        fun `changes the book status and send a event`() {
+            val book = buildBook(123)
+            every { applicationEventPublisher.publishEvent(any()) } just runs
+
+            assertAll(
+                    { assertNotNull(book.saleDate) },
+                    { assertNotNull(book.status) },
+                    { verify(exactly = 1) { applicationEventPublisher.publishEvent(any()) } }
+            )
+        }
+    }
+
+    private fun buildBook(bookId: Int?, name: String = "Example book", bookStatus: BookStatus = BookStatus.ATIVO) =
+        BookModel(
+            bookId,
+                name,
+                BigDecimal.TEN,
+                buildCustomer(),
+                bookStatus,
+                null
+        )
+
+    private fun buildCustomer() =
+        CustomerModel(
+                123,
+                "Some Custoemr",
+                "some@customer.com",
+                mutableSetOf(Profiles.CUSTOMER)
+        )
+}
