df9e1badc236cfb7c3720659483f96e77b0fafdc
Gustavo Pinoti <gustavopinoticel@gmail.com>
2021-06-17 13:58:45 -0300

    Ajuste Purchase Controller

--- src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/PostPurchaseRequest.kt
@@ -1,8 +1,12 @@
 package com.mercadolivro.controller.request
 
+import com.fasterxml.jackson.annotation.JsonAlias
+
 class PostPurchaseRequest (
 
+    @JsonAlias("customer_id")
     val customerId: Int,
 
+    @JsonAlias("book_ids")
     val bookIds: Set<Int>
 )
