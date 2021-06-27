2e41cc058b8a435747970a599c9ab3736bd2a6ea
daniel <daniel_donato10@yahoo.com.br>
2021-06-27 20:32:43 -0300

    Empty constructor to CredentialRequest

--- src/main/kotlin/com/mercadolivro/controller/request/CredentialRequest.kt
+++ src/main/kotlin/com/mercadolivro/controller/request/CredentialRequest.kt
@@ -1,6 +1,10 @@
 package com.mercadolivro.controller.request
 
 data class CredentialRequest(
-    val email: String,
-    val password: String
-)
+    val email: String?,
+    val password: String?
+) {
+    constructor() : this(null, null) {
+
+    }
+}
